06c96dd7c700fc5084d3d771c4d78d1b0a15ccaf
Mateus Neres <mateusneres03@gmail.com>
2024-06-18 08:52:50 -0300

    beecrowd 1005 	Média 1

---
+++ 1005.py
@@ -0,0 +1,10 @@
+peso_A = 3.5
+peso_B = 7.5
+soma_pesos = peso_A + peso_B
+
+nota_A = float(input())
+nota_B = float(input())
+
+media = ((nota_A * peso_A) + (nota_B * peso_B)) / soma_pesos
+
+print(f"MEDIA = {media:.5f}")
