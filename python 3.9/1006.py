4518abf49b3c832695703c7e97ea12d54d89029b
Mateus Neres <mateusneres03@gmail.com>
2024-06-18 10:24:12 -0300

    beecrowd 1006 Média 2

---
+++ python 3.9/1006.py
@@ -0,0 +1,12 @@
+a = float(input())
+b = float(input())
+c = float(input())
+
+peso_A = 2
+peso_B = 3
+peso_C = 5
+soma_peso = peso_A + peso_B + peso_C
+
+media = ((a * peso_A) + (b * peso_B) + (c * peso_C)) / soma_peso
+
+print(f"MEDIA = {media:.1f}")
