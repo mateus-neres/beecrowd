c2686ba0b917a41f7881789a67ce92fb387ced39
Mateus Neres <mateusneres03@gmail.com>
2024-06-25 17:32:17 -0300

    Notas e Moedas

--- 1021.py
+++ 1021.py
@@ -3,16 +3,20 @@ x = float(input())
 cedulas = [100.00,50.00,20.00,10.00,5.00,2.00]
 moedas = [1.00,0.50,0.25,0.10,0.05,0.01]
 
+centavos = int(x * 100)
+
 print("NOTAS:")
 
-for cedeula in cedulas:
-    decomposicao = x // cedeula
-    x = round(x % cedeula, 2)
-    print(f"{decomposicao:.0f} nota(s) de R$ {cedeula:.2f}")
+for cedula in cedulas:
+    valor_centavos = int(cedula * 100)
+    decomposicao = centavos // valor_centavos
+    centavos = centavos % valor_centavos
+    print(f"{decomposicao:.0f} nota(s) de R$ {cedula:.2f}")
 
 print("MOEDAS:")
 
 for moeda in moedas:
-    decomposicao = x // moeda
-    x = round(x % moeda,2)
+    valor_centavos = int(moeda * 100)
+    decomposicao = centavos // valor_centavos
+    centavos = centavos % valor_centavos
     print(f"{decomposicao:.0f} moeda(s) de R$ {moeda:.2f}")
