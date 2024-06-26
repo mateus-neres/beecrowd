4bb7d97256e94fb908d096c8e5b83cff12a5f4f8
Mateus Neres <mateusneres03@gmail.com>
2024-06-26 09:25:52 -0300

    Lanche

---
+++ 1038.py
@@ -0,0 +1,16 @@
+codigo, quantidade = input().split()
+codigo = int(codigo)
+quantidade = int(quantidade)
+
+lista = [
+    [1,'Cachorro Quente', 4.00],
+    [2, 'X-Salada',4.50],
+    [3,'XBacon',5.00],
+    [4,'Torrada Simples',2.00],
+    [5,'Refrigerante',1.50]
+]
+total_a_pagar = 0
+for indice in range(len(lista)):
+    if lista[indice][0] == codigo:
+        total_a_pagar += lista[indice][2] * quantidade
+print(f'Total: R$ {total_a_pagar:.2f}')
