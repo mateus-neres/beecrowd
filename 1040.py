4e5ca92f3007a57d8159f88b3e5d1442793a0ae3
Mateus Neres <mateusneres03@gmail.com>
2024-06-26 10:04:30 -0300

    Média 3

---
+++ 1040.py
@@ -0,0 +1,31 @@
+n1, n2, n3, n4 = input().split()
+n1 = float(n1)
+n2 = float(n2)
+n3 = float(n3)
+n4 = float(n4)
+
+peso_n1 = 2
+peso_n2 = 3
+peso_n3 = 4
+peso_n4 = 1
+
+soma_peso = peso_n1 + peso_n2 + peso_n3 + peso_n4
+
+media = ((peso_n1 * n1) + (peso_n2 * n2) + (peso_n3 * n3) + (peso_n4 * n4)) / soma_peso
+print(f'Media: {media:.1f}')
+
+if media >= 7:
+    print('Aluno aprovado.')
+elif media < 5:
+    print('Aluno reprovado.')
+else:
+    print('Aluno em exame.')
+    n5 = float(input())
+    print(f'Nota do exame: {n5}')
+    media_final = (media + n5) / 2
+    if media_final >= 5:
+        print('Aluno aprovado.')
+        print(f'Media final: {media_final:.1f}')
+    else:
+        print('Aluno reprovado.')
+        print(f'Media final: {media_final:.1f}')
