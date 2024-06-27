1f577fb26cbbefe5bdf27573d115f1f9da60fc7a
Mateus Neres <mateusneres03@gmail.com>
2024-06-26 22:10:21 -0300

    Sort Simples

---
+++ INICIANTE/1042SortSimples.py
@@ -0,0 +1,14 @@
+a, b, c = map(int, input().strip().split())
+
+lista = [a,b,c]
+
+valores_sorted = sorted(lista)
+
+for valores in valores_sorted:
+    print(valores)
+
+print()
+
+print(a)
+print(b)
+print(c)
