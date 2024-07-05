bc28524b87a1ed9c3954deeaef115f821dc8dd13
Mateus Neres <mateusneres03@gmail.com>
2024-07-04 21:41:33 -0300

    1047

--- INICIANTE/1047TempoDeJogoComMinutos.py
+++ INICIANTE/1047TempoDeJogoComMinutos.py
@@ -1,23 +1,14 @@
 hora_inicio, minuto_inicio, hora_final, minuto_final = map(int, input().strip().split())
 
-uma_hora = 60
-um_dia = 24
-
-# Convertendo os horários de início e fim para minutos desde a meia-noite
-total_minutos_inicio = hora_inicio * uma_hora + minuto_inicio
-total_minutos_final = hora_final * uma_hora + minuto_final
-
-# Calculando a diferença em minutos
-if total_minutos_inicio <= total_minutos_final:
-    duracao_em_minutos = (total_minutos_final + (um_dia * uma_hora)) - total_minutos_inicio
-    if duracao_em_minutos // um_dia >= um_dia * uma_hora:
-        duracao_em_minutos = total_minutos_final - total_minutos_inicio
+if hora_inicio >= hora_final:
+    horas_para_minuto_inicio = ((hora_inicio + 24) * 60) + minuto_inicio
+    horas_para_minuto_final = (hora_final * 60) + minuto_final
+    hora =  (horas_para_minuto_inicio - horas_para_minuto_final) // 60
+    minuto = (horas_para_minuto_inicio % horas_para_minuto_final) // 60
 else:
-    duracao_em_minutos = (um_dia * uma_hora) - (total_minutos_inicio - total_minutos_final)
+    horas_para_minuto_inicio = (hora_inicio * 60) + minuto_inicio
+    horas_para_minuto_final = (hora_final * 60) + minuto_final
+    hora =  (horas_para_minuto_final - horas_para_minuto_inicio) // 60
+    minuto = (horas_para_minuto_final % horas_para_minuto_inicio)
 
-# Convertendo a diferença de minutos para horas e minutos
-duracao_horas = duracao_em_minutos // uma_hora
-duracao_minutos = duracao_em_minutos % uma_hora
-
-# Saída formatada
-print(f"O JOGO DUROU {duracao_horas} HORA(S) E {duracao_minutos} MINUTO(S)")
+print(f"O JOGO DUROU {hora} HORA(S) E {minuto} MINUTO(S)")
