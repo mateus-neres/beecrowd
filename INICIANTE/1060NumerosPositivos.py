1c530a0dcb74409dc870dbe97cc36acbe694f5fe
Mateus Neres <mateusneres03@gmail.com>
2024-07-16 13:48:33 -0300

    1060

--- INICIANTE/1060NumerosPositivos.py
+++ INICIANTE/1060NumerosPositivos.py
@@ -1,11 +1,8 @@
-lista = []
+contador = 0
+
 for i in range(6):
     a = float(input())
-    lista.append(a)
-
-contador = 0
-for i in lista:
-    if i > 0:
+    if a > 0:
         contador += 1
 
 print(f'{contador} valores positivos')
