5a0dfc8f5c83ced0cdefe99e03d92e6b26813ace
Mateus Neres <mateusneres03@gmail.com>
2024-08-14 19:14:04 -0300

    .

--- INICIANTE/1047TempoDeJogoComMinutos.py
+++ INICIANTE/1047TempoDeJogoComMinutos.py
@@ -1,14 +1,16 @@
 hora_inicio, minuto_inicio, hora_final, minuto_final = map(int, input().strip().split())
 
-if hora_inicio >= hora_final:
-    horas_para_minuto_inicio = ((hora_inicio + 24) * 60) + minuto_inicio
-    horas_para_minuto_final = (hora_final * 60) + minuto_final
-    hora =  (horas_para_minuto_inicio - horas_para_minuto_final) // 60
-    minuto = (horas_para_minuto_inicio % horas_para_minuto_final) // 60
+minutos_inicio = (hora_inicio * 60) + minuto_inicio
+minutos_final = (hora_final * 60) + minuto_final
+
+if minutos_inicio > minutos_final:
+
+    duracao = (1440 - minutos_inicio) + minutos_final
 else:
-    horas_para_minuto_inicio = (hora_inicio * 60) + minuto_inicio
-    horas_para_minuto_final = (hora_final * 60) + minuto_final
-    hora =  (horas_para_minuto_final - horas_para_minuto_inicio) // 60
-    minuto = (horas_para_minuto_final % horas_para_minuto_inicio)
+
+    duracao = minutos_final - minutos_inicio
+
+hora = duracao // 60
+minuto = duracao % 60
 
 print(f"O JOGO DUROU {hora} HORA(S) E {minuto} MINUTO(S)")
