1845bc7f841ce2f89b7334a414a6ffe5fd21ae6e
Mateus Neres <mateusneres03@gmail.com>
2024-08-26 22:23:16 -0300

    2605

--- SQL/2605RepresentantesExecutivos.sql
+++ SQL/2605RepresentantesExecutivos.sql
@@ -1,4 +1,5 @@
+-- Active: 1723675705494@@127.0.0.1@5432@products
 SELECT products.name, providers.name
-FROM products
-JOIN providers ON products.id_providers = providers.id
-WHERE products.id_categories = 6;
+FROM products, providers
+WHERE products.id_providers = providers.id
+and products.id_categories = 6;
