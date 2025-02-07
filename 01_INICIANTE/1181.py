f931f20cabd096869cf84214d41da408aa2eab14
mateus-neres <mateusneres03@gmail.com>
2025-02-07 20:10:51 -0300

    1181

---
+++ 01_INICIANTE/1181.py
@@ -0,0 +1,19 @@
+L = int(input())
+T = input().strip()
+
+M = []
+
+for i in range(12):
+    linha = []
+    for j in range(12):
+        valor = float(input())
+        linha.append(valor)
+    M.append(linha)
+
+soma = sum(M[L])
+
+if T == 'S':
+    print(f"{soma:.1f}")
+elif T == 'M':
+    media = soma / 12
+    print(f"{media:.1f}")
